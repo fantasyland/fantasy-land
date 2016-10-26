1001d46f2af89e36a861e9bfdd41b537f4919f65
Roman Pominov <rpominov@gmail.com>
2016-10-26 16:43:02 +0300

    simplify laws/applicative.js

--- laws/applicative.js
+++ laws/applicative.js
@@ -1,36 +1,26 @@
 'use strict';
 
-const {identity, thrush} = require('fantasy-combinators');
 const {of, ap} = require('..');
 
-/**
+const identity = eq => (A, v) => {
+  return eq(
+    v[ap](A[of](x => x)),
+    v
+  );
+}
 
-### Applicative
+const homomorphism = eq => (A, x, f) => {
+  return eq(
+    A[of](x)[ap](A[of](f)),
+    A[of](f(x))
+  );
+}
 
-1. `v.ap(A.of(x => x))` is equivalent to `v` (identity)
-2. `A.of(x).ap(A.of(f))` is equivalent to `A.of(f(x))` (homomorphism)
-3. `A.of(y).ap(u)` is equivalent to `u.ap(A.of(f => f(y)))` (interchange)
+const interchange = eq => (A, y, u) => {
+  return eq(
+    A[of](y)[ap](u),
+    u[ap](A[of](f => f(y)))
+  );
+}
 
-**/
-
-const identityʹ = T => eq => x => {
-  const a = T[of](x)[ap](T[of](identity));
-  const b = T[of](x);
-  return eq(a, b);
-};
-
-const homomorphism = T => eq => x => {
-  const a = T[of](x)[ap](T[of](identity));
-  const b = T[of](identity(x));
-  return eq(a, b);
-};
-
-const interchange = T => eq => x => {
-  const u = T[of](identity);
-
-  const a = T[of](x)[ap](u);
-  const b = u[ap](T[of](thrush(x)));
-  return eq(a, b);
-};
-
-module.exports = {identity: identityʹ, homomorphism, interchange};
+module.exports = {identity, homomorphism, interchange};
