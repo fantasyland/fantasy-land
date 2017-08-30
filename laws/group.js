6f8e2f2d4e0534fdc29c7583ddf5d60d59fff575
Ian Hofmann-Hicks (evil) <evilsoft@aol.com>
2017-08-30 10:12:32 -0700

    back to inverse

--- laws/group.js
+++ laws/group.js
@@ -6,12 +6,12 @@ const {of, empty, concat, invert} = require('..');
 
 ### Group
 
-1. `g.concat(g.invert())` is equivalent to `g.empty()` (right cancellation)
-2. `g.invert().concat(g)` is equivalent to `g.empty()` (left cancellation)
+1. `g.concat(g.invert())` is equivalent to `g.empty()` (right inverse)
+2. `g.invert().concat(g)` is equivalent to `g.empty()` (left inverse)
 
 **/
 
-const rightCancellation = T => eq => x => {
+const rightInverse = T => eq => x => {
   const g = T[of](x);
 
   const a = g[concat](g[invert]());
@@ -19,7 +19,7 @@ const rightCancellation = T => eq => x => {
   return eq(a, b);
 };
 
-const leftCancellation = T => eq => x => {
+const leftInverse = T => eq => x => {
   const g = T[of](x);
 
   const a = g[invert]()[concat](g);
@@ -27,4 +27,4 @@ const leftCancellation = T => eq => x => {
   return eq(a, b);
 };
 
-module.exports = {rightCancellation, leftCancellation};
+module.exports = {rightInverse, leftInverse};
